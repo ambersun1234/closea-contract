ee449758975c2244067a8df7d71a5034d1c69b42
Shawn Hsu <ambersun1019.shawn@gmail.com>
2023-04-10 02:18:10 +0800

    Init hardhat project

---
+++ hardhat.config.ts
@@ -0,0 +1,45 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
+import "hardhat-deploy";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+import "hardhat-contract-sizer";
+
+import {
+    EtherscanAPIKey,
+    PrivateKey,
+    SepoliaRPCUrl
+} from "./utils/env";
+
+const config: HardhatUserConfig = {
+    solidity: {
+        compilers: [{ version: "0.8.18" }, {version: "0.6.6"}]
+    },
+    networks: {
+        sepolia: {
+            url: SepoliaRPCUrl || "",
+            accounts: [PrivateKey],
+            chainId: 11155111,
+            gasPrice: 5 * 1000000000
+        }
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0
+        }
+    },
+    etherscan: {
+        apiKey: EtherscanAPIKey
+    },
+    mocha: {
+        bail: true,
+        timeout: 300000
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD"
+    }
+};
+
+export default config;
