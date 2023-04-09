0c509c1a938f636d2cd24b2576a0ed1e5a190d5d
Shawn Hsu <ambersun1019.shawn@gmail.com>
2023-04-10 02:30:18 +0800

    Remove goerli rpc url from environment variable

--- utils/env.ts
+++ utils/env.ts
@@ -1,4 +1,3 @@
-export const GoerliRPCUrl = process.env.GOERLI_RPC_URL;
 export const SepoliaRPCUrl = process.env.SEPOLIA_RPC_URL;
 export const EtherscanAPIKey = process.env.ETHERSCAN_API_KEY;
 export const PrivateKey = process.env.PRIVATE_KEY || "";
