7729c709b0c9df4655feb608ced33839620c468f
Shawn Hsu <ambersun1019.shawn@gmail.com>
2023-04-10 13:02:19 +0800

    Add deploy scripts

---
+++ deploy/deployMarketplace.ts
@@ -0,0 +1,21 @@
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const deployMarketPlace: DeployFunction = async (
+  hre: HardhatRuntimeEnvironment
+) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deployer } = await getNamedAccounts();
+  const { deploy } = deployments;
+
+  await deploy("Marketplace", {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: 1,
+  });
+};
+
+deployMarketPlace.tags = ["marketplace"];
+
+export default deployMarketPlace;
